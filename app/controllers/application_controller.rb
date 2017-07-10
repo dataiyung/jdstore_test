c60327ff01dcc4eba8133490378944133a6fdfbe
dataiyung <dataiyung@gmail.com>
2017-07-10 16:11:02 +0800

    only admin can access backend panel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+def admin_required
+  if !current_user.admin?
+    redirect_to "/", alert: "你壞壞，你不是管理員喔！"
+  end
+end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+u = User.new
+u.email = "dataiyung@gmail.com"
+
+u.password ="1qa1qa"
+
+u.password_confirmation ="1qa1qa"
+
+u.is_admin = true
+u.save
