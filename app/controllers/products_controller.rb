65821dbe4fd8eec935d49e8349668753277ec4af
dataiyung <dataiyung@gmail.com>
2017-07-11 23:11:01 +0800

    cannot add item with quantity 0

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,7 @@ end
 def add_to_cart
   @product =Product.find(params[:id])
   if !current_cart.products.include?(@product)
+    current_cart.add_product_to_cart(@product)
 flash[:cotice]= "你已成功將 #{@product.title} 加入購物車"
 else
   flash[:warning] = "您的購物車已有此物品"
